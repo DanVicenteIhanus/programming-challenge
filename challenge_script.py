bf5bdea3b0d3001a01e49ee339106f4467ec9fff
DanVicenteIhanus <dan.vicente.ihanus@gmail.com>
2023-10-21 11:43:27 +0200

    fixed NN

--- challenge_script.py
+++ challenge_script.py
@@ -14,6 +14,8 @@ from sklearn.preprocessing import LabelEncoder
 from scipy import stats
 import tensorflow as tf
 import keras
+from keras import optimizers
+from keras.utils import to_categorical
 
 
 '''
@@ -39,6 +41,9 @@ def clean_data(data: pd.DataFrame):
     labels.replace('Bobborg', 'Boborg',inplace=True)
     labels.replace('Jorggsuto', 'Jorgsuto', inplace=True)
     labels.replace('Atsutoob', 'Atsutobob', inplace=True)
+    labels.replace('Boborg', 0, inplace=True)
+    labels.replace('Jorgsuto', 1, inplace=True)
+    labels.replace('Atsutobob',2, inplace=True)
     return features, labels, len(data.index)
 
 def compute_PCA(data):
@@ -64,8 +69,33 @@ TODO: Pick some other model (NN?)
 ------------------------------------
 '''
 
-def ann_routine(features, labels, K, M):
-    return
+def ann_routine(features, labels, K1, K2, a1, a2, k1_init, k2_init, opt, Nbatch, epochs):
+    one_hot_labels = to_categorical(labels, num_classes=3)
+    input_layer = tf.keras.Input(shape=features.shape[1:])
+    Z1 = keras.layers.Dense(units=K1, activation=a1, use_bias=True,
+                            kernel_initializer=k1_init,
+                            bias_initializer='zeros',
+                            name='hidden_layer_1')
+    Z2 = keras.layers.Dense(units=K2, activation=a2, use_bias=True,
+                            kernel_initializer=k2_init,
+                            bias_initializer='zeros',
+                            name='hidden_layer_2')
+    
+    output_layer = keras.layers.Dense(units=3, 
+                                      activation='softmax',
+                                        name='output_layer')
+
+    model = keras.models.Sequential([input_layer, Z1, Z2, output_layer])
+    
+    model.compile(optimizer=opt, 
+                  loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    
+    pred = model.fit(x=features, y=one_hot_labels, batch_size=Nbatch,
+                     epochs=epochs,
+                     validation_split=0.3,
+                     verbose=1)
+    return pred
 
 def svm_routine(train_features, train_labels, 
                 test_features, 
@@ -105,10 +135,7 @@ if __name__ == '__main__':
     features, labels, N = clean_data(training_data) # clean data
     pca_features = compute_PCA(features)            # compute PCA 
 
-    # --- Hyperparameters --- #
-
-
-    # Verify amount of classes
+    # --- Visualize classes --- #
     print('================ Class distribution ==================')
     classes = labels.unique()
     print(f'Available classes: {classes}')
@@ -116,4 +143,20 @@ if __name__ == '__main__':
         print(f'Amount in class {cl}: {labels.value_counts()[cl]}')
     print('======================================================')
 
+    # --- Hyperparameters --- #
+    K1 = 10 # Nodes in layer 1
+    K2 = 10 # Nodes in layer 2
+    a1 = 'sigmoid'
+    a2 = 'relu'
+    k1_init = 'random_uniform'
+    k2_init = 'random_normal'
+    epochs = 100 # EPOCHS
+    NBatch = 50
+    learning_rate = 0.01
+    opt = optimizers.Adamax(learning_rate=learning_rate)
+    
+    blahonga = ann_routine(pca_features, labels, K1=K1,
+                 K2=K2, opt=opt, a1=a1, a2=a2, 
+                 k1_init=k1_init, k2_init=k2_init,
+                 Nbatch=NBatch, epochs=epochs)
     
