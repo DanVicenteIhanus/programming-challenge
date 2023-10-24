17c58fe2a37eb9f953516204e024bc827a94b769
DanVicenteIhanus <dan.vicente.ihanus@gmail.com>
2023-10-24 23:03:35 +0200

    added resampling et.c

--- challenge_script.py
+++ challenge_script.py
@@ -13,6 +13,8 @@ from keras import optimizers
 from keras.utils import to_categorical
 from matplotlib import pyplot as plt
 from scipy.stats import entropy
+from imblearn.over_sampling import RandomOverSampler
+
 
 '''
 ------------------------------------
@@ -44,31 +46,36 @@ def clean_data(data: pd.DataFrame, training: bool):
     encoded_col = le.fit_transform(features['x7'])
     features['x7'] = encoded_col
     features = features.fillna(0)
-    features = features.drop(['x2', 'x12'], axis=1)
+    features = features.drop(['x12'], axis=1)
 
     return features, labels, len(data.index)
 
 def handle_data(datadir, training_set, test_set):
+    
+    # --- import and clean data
     training_data = import_data(datadir+training_set)
     test_data = import_data(datadir+test_set)
     features, labels, N = clean_data(training_data, True)
     test_features, _, Ntest = clean_data(test_data, False)
-    print(f'Size of test set: {Ntest}')
-    print(f'Size of training set: {N}')
-    features.drop(labels='x10', axis=1)
-    test_features.drop(labels='x10', axis=1)
-    pca_test_features = compute_PCA(test_features) 
-    pca_features = compute_PCA(features)           
-    return pca_features, labels, N, pca_test_features
 
-def compute_PCA(data):
+    # --- oversample class 1 --- #
+    oversampler = RandomOverSampler(sampling_strategy='auto', random_state=93)
+    features_resampled, labels_resampled = oversampler.fit_resample(features, labels)
+    pca_features, pca_test_features = compute_PCA(features_resampled, test_features)
+    print(f'Size of resampled set: {len(labels_resampled)}')
+    return pca_features, labels_resampled, N, pca_test_features, 
+
+def compute_PCA(data, test_data):
     # perform PCA on normalized data and see how many dims are kepts
     std_scaler = StandardScaler()
     scaled_data = std_scaler.fit_transform(data)
     pca = PCA(n_components = 'mle', svd_solver='auto')
-    pca_data = pca.fit_transform(scaled_data)
-    print(np.shape(pca_data))
-    return pca_data
+    pca.fit(scaled_data)
+
+    pca_data = pca.transform(scaled_data)
+    scaled_test_data = std_scaler.fit_transform(test_data)
+    pca_test_data = pca.transform(scaled_test_data)
+    return pca_data, pca_test_data
 
 def plot_histograms(pca_features, pca_test_features):
     # --- Visualize feature distributions --- #
@@ -136,7 +143,7 @@ def svm_routine(train_features, train_labels,
     # --- fit data and predict --- #
     svm_model.fit(train_features, train_labels)
     predicted_labels = svm_model.predict(test_features)
-
+    
     return predicted_labels
 
 def compute_accuracy(predicted, true):
@@ -150,7 +157,10 @@ Main script
 '''
 
 if __name__ == '__main__':
-    
+    seed = 93
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
     # --- Handle Data --- #
     datadir = '/Users/danvicente/Datalogi/DD2421 - Maskininlärning/programming challenge/'
     training_set = 'TrainOnMe.csv'
@@ -169,7 +179,6 @@ if __name__ == '__main__':
     #plot_histograms(pca_features, pca_test_features)
 
     # --- Hyperparameters --- #
-    
     # NN
     K1 = 40; K2 = 25; K3 = 30
     a1 = 'relu'; a2 = 'tanh'; a3 = 'sigmoid'
@@ -185,7 +194,9 @@ if __name__ == '__main__':
                             opt=opt, Nbatch=NBatch, epochs=epochs)
     
     # --- 10-fold cross validation --- #
-    skf = StratifiedKFold(n_splits = 5, shuffle=True)
+
+    '''
+    skf = StratifiedKFold(n_splits = 10, shuffle=True)
     cv_svm = cross_val_score(SVC(kernel=kernel, gamma=gamma), X=pca_features, y=labels, cv=skf, verbose=1)
     print(f'Accuracy of SVM: {np.mean(cv_svm)}') 
     
@@ -197,16 +208,29 @@ if __name__ == '__main__':
         accuracy = compute_accuracy(np.argmax(y_pred, axis=1), labels[test_indices])
         acc.append(accuracy)
     print(f'Accuracy of ANN: {np.mean(acc)}')
+    '''
 
-    # --- Train model --- # 
-    '''
+    # --- Train models --- # 
     ann_model = ann_routine(pca_features, labels, K1=K1, K2=K2, K3=K3, a1=a1, a2=a2, a3=a3,
-                            k1_init=k1_init, k2_init=k2_init, k3_init=k3_init,
-                            opt=opt, Nbatch=NBatch, epochs=epochs)
-    ann_model.fit(pca_features, labels, to_categorical(labels[train_indices], num_classes=3),
-                  batch_size=NBatch, verbose=1)
-    '''
-    # --- Predict --- #
+                        k1_init=k1_init, k2_init=k2_init, k3_init=k3_init,
+                        opt=opt, Nbatch=NBatch, epochs=epochs)
     
-    #TODO: NEED TO HANDLE THE test-data as well. 
-    #ann_model.predict(test_data)
+    ann_model.fit(pca_features, to_categorical(labels, num_classes=3), verbose=1, batch_size=NBatch)
+    svm_model = SVC(kernel=kernel, gamma=gamma)
+    svm_model.fit(pca_features, labels)
+    
+    # --- Predict --- #
+
+    class_dict = {0: 'Boborg', 1: 'Jorgsuto', 2: 'Atsutobob'}
+    y_pred = ann_model.predict(pca_test_features)
+    classifications = np.argmax(y_pred, axis=1)
+    predicted_classes = [class_dict.get(class_num, 'Unknown') for class_num in classifications]
+    svm_y_pred = svm_model.predict(pca_test_features)
+    
+    # --- save classifications --- #
+    with open('predicted_classes.txt', 'w') as output:
+        for pred in predicted_classes:
+            output.write(str(pred)+'\n')
+    with open('svm_predicted_classes.txt', 'w') as output:
+        for pred in svm_y_pred:
+            output.write(str(pred)+'\n')
